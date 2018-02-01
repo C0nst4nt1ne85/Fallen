f07b76435725840b85ed0a4347947087f7a9df45
Constantine Melios <djagil@gmail.com>
2018-02-01 18:15:02 +0200

    debugged

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
 
 $(document).ready(function(){
    $(".titles").on("click", function() {
-       $(".focus").removeClass("focus");
-       $(this).addClass("focus");
-       $(this).find(".info").slideToggle();
+       if(!$(this).hasClass("focus")) {
+            $(".focus").removeClass("focus");
+            $(".titles").find(".info").slideUp();
+            $(this).toggleClass("focus");
+            $(this).find(".info").slideDown();
+       }
+       
    })
 });
